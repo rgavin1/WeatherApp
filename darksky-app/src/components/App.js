412c62427a675263b9b9665a25f6155745c8ee62
rgavin1 <44215916+rgavin1@users.noreply.github.com>
2019-06-28 09:19:37 -0400

    Weekly Component Created

--- darksky-app/src/components/App.js
+++ darksky-app/src/components/App.js
@@ -3,12 +3,14 @@ import logo from '../logo.svg';
 import '../Styles/App.css';
 import Search from './Search';
 import Daily from './Daily';
+import Weekly from './Weekly';
 
 function App() {
   return (
       <main>
         <Search />
         <Daily />
+        <Weekly />
       </main>
   );
 }
---
+++ darksky-app/src/components/Weekly.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Weekly extends Component {
+  render(){
+    return(
+      <div>Weekly</div>
+    );
+  }
+}
+
+export default Weekly;
