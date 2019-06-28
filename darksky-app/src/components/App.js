e8d83de7755ad41bf0bd1d64bbf7a0e7f91bd5ec
rgavin1 <44215916+rgavin1@users.noreply.github.com>
2019-06-28 09:11:22 -0400

    Create Search Component

--- darksky-app/src/components/App.js
+++ darksky-app/src/components/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import logo from '../logo.svg';
 import '../Styles/App.css';
+import Search from './Search';
 
 function App() {
   return (
-    
+    <Search />
   );
 }
 
